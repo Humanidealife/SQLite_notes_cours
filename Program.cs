077b48787f631d6fdf8e1acf5aa6e8a214f45d6a
qianwang <wangqianlf@live.com>
2024-03-10 13:51:47 +0100

    initialisation du projet avec package installé

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-namespace SQLite
+namespace SQLite_notes_cours
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.ReadKey();
+    }
+  }
+}
