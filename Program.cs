136b7d6925c52d9973fa164afece01b34b7c3ef7
qianwang <wangqianlf@live.com>
2024-03-10 16:02:37 +0100

    ajout des espaces de nom. Création de DB SQLite, création connection, stacker requête, création command, exécution command

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
+using System; // espace de nom
+using System.IO; // permet de gérer la création des fichiers
+using System.Data.SQLite; // permet de gérer la base de données
+
+
 
 namespace SQLite_notes_cours
 {
@@ -7,6 +11,38 @@ namespace SQLite_notes_cours
   {
     static void Main(string[] args)
     {
+      // création de la base de données
+      // pour tester si la base de données existe
+      string dbPath = "database.sqlite";
+      if(!File.Exists(dbPath)) CreateDB();
+
+      // création de la DB
+      void CreateDB()
+      {
+        // création du ficher de la base de données
+        SQLiteConnection.CreateFile(dbPath);
+
+        // création de la coonextion à la base de données
+        SQLiteConnection con = new SQLiteConnection("Data Source=database.sqlite;Veriosn=3;"); // la chaîne d'identification de la base de données
+
+        // ouverture de la connextion
+        con.Open();
+
+        // des requêtes SQL
+        // dans le string sql on met le code SQL pour des requêtes
+        // Création d'une table Clients
+        string sql = "CREATE TABLE Clients (nom varchar(20), prenom varchar(20))";
+
+        // création d'une command afin de lancer la requête
+        SQLiteCommand command = new SQLiteCommand(sql, con);
+
+        // exécution de la command
+        command.ExecuteNonQuery();
+
+        // fermeture de la connexion
+        con.Close();
+      }
+
       Console.ReadKey();
     }
   }
